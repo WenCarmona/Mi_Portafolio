34f3fb1f5504241bc6aa526203efd906a963ee69
WenCarmona <wencarmona24@hotmail.com>
2024-11-14 20:16:24 -0500

    Corrección
    Corrección de código

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import Navbar from './components/Navbar'
+import Hero from './components/Hero'
+import Portfolio from './components/Portfolio'
+import Skills from './components/Skills'
+import Experience from './components/Experience'
+import Contact from './components/Contact'
+import Footer from './components/Footer'
+
+export default function App() {
+  return (
+    <>
+      <Navbar />
+      <Hero />
+      <Skills />
+      <Portfolio />
+      <Experience />
+      <Contact />
+      <Footer />
+
+
+    </>
+  )
+}
+
